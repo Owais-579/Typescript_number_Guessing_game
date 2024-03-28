c0c51196ee519d96a62c38540ccdd04b87e90159
Owais-579 <142677648+Owais-579@users.noreply.github.com>
2024-03-28 08:12:16 -0700

    add  files

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "Userguessednum",
+        type: "number",
+        message: "Please Enter your number"
+    }]);
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber);
+if (randomNumber === answer.Userguessednum) {
+    console.log("Congratulation You Enter correct Number");
+}
+else {
+    console.log("You Enter wrong number please Try Again");
+}
